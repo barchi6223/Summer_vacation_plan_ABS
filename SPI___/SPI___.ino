fed50b896a29066e984ec3da59a59f8059a26448
barchi <barchi6223@gamil.com>
2018-07-11 22:03:32 +0800

    greg

--- SPI___/SPI___.ino
+++ SPI___/SPI___.ino
@@ -1,20 +1,8 @@
-#define CS 10
-#define SDO 11
-#define SDI 12
-#define SCK 13
+
 void setup() {
   Serial.begin(9600);
-  pinMode(SDI,INPUT);
-  pinMode(SDO,OUTPUT);
-  pinMode(CS,OUTPUT);
-  pinMode(SCK,OUTPUT);
   
-  digitalWrite(CS,HIGH);
 }
 void loop() {
-  digitalWrite(CS,LOW);
-  if(digitalRead(SDI)>0){
-  Serial.println(SDI);
-  }
-  digitalWrite(CS,HIGH);
+  Serial.println(123dssdfsdfsd);
 }
